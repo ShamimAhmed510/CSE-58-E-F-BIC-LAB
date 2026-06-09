4cb303ccc9d04445b9eb67d73a8301e087f4bb26
Shamim Ahmed Raju <140370926+ShamimAhmed510@users.noreply.github.com>
2026-06-09 16:15:33 +0600

    Count and print most frequent substrings of length k

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,15 @@
 
+text = input().strip()
+k = int(input())
+
+freq = {}
+
+for i in range(len(text) - k + 1):
+    pat = text[i:i+k]
+    freq[pat] = freq.get(pat, 0) + 1
+
+mx = max(freq.values())
+
+for key in freq:
+    if freq[key] == mx:
+        print(key, end=" ")
