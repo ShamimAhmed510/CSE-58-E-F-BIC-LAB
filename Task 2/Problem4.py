86c5f37f67b7d9060939b3a1ec57d15fee01371c
Shamim Ahmed Raju <140370926+ShamimAhmed510@users.noreply.github.com>
2026-06-09 16:12:43 +0600

    Update Problem4.py

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,12 @@
 
+pattern = input().strip()
+genome = input().strip()
+
+k = len(pattern)
+res = []
+
+for i in range(len(genome) - k + 1):
+    if genome[i:i+k] == pattern:
+        res.append(str(i))
+
+print(" ".join(res))
