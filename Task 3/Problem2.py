3ae38f73e5a36a25efd18862ec4bb1c2f6791696
Shamim Ahmed Raju <140370926+ShamimAhmed510@users.noreply.github.com>
2026-06-16 16:13:19 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,23 @@
 
+def mismatch(s1 ,s2):
+    return sum(c1 != c2 for c1, c2 in zip(s1,s2))
+
+def approximate_pattern_matching(pattern, text , d):
+    k = len(pattern)
+    positions = []
+
+    for i in range(len(text) - k + 1):
+        window = text[i:i+k]
+        if mismatch(pattern,  window) <= d:
+            positions.append(i)
+    
+    return positions
+
+pattern = input().strip()
+text = input().strip()
+d = int(input().strip())
+
+result = approximate_pattern_matching(pattern, text, d)
+
+print(*result)
+
